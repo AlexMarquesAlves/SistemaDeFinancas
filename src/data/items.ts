d31693562086610a159058f67d164d9783f083ec
AlexMarqueAlves <atanaelleonardo@gmail.com>
2021-10-17 21:12:26 -0300

    iniciando com itens

---
+++ src/data/items.ts
@@ -0,0 +1,6 @@
+export const items = [
+  { date: new Date(2021, 9, 15), category: 'food', title: 'McDonalds', value: 32.12 },
+  { date: new Date(2021, 9, 15), category: 'food', title: 'Burger King', value: 28 },
+  { date: new Date(2021, 9, 16), category: 'Rent', title: 'Aluguel Apt', value: 2300 },
+  { date: new Date(2021, 10, 18), category: 'salary', title: 'Salário ACME', value: 4500 },
+];
